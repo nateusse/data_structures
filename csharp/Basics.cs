e59030b51692bdcec214be25c494a3a53f6521be
nateusse <natyeusse@gmail.com>
2023-12-01 18:25:36 -0500

    pushing initial files

---
+++ csharp/Basics.cs
@@ -0,0 +1,7 @@
+public class Basics
+{
+    public static void Hola()
+    {
+        Console.WriteLine("Hello, World deesde basics!");
+    }
+}
---
+++ csharp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        Basics.Hola();
+    }
+}
