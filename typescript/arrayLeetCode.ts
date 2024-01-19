ecd095343c2bda55d028f3a544226bb027f04994
nateusse <natyeusse@gmail.com>
2024-01-19 16:56:47 -0500

    anagram ts

--- typescript/arrayLeetCode.ts
+++ typescript/arrayLeetCode.ts
@@ -2,7 +2,7 @@
     Given an integer array nums,
     return true if any value appears at least twice in the array, 
     and return false if every element is distinct.
-*/
+
 function containsDuplicate(nums: number[]): boolean {
     for (let i:number = 0; i < nums.length - 1 ; i++){
         for (let j:number = i + 1; j < nums.length ; j++){
@@ -16,6 +16,8 @@ function containsDuplicate(nums: number[]): boolean {
 
 console.log(containsDuplicate([1,2,3,1])) //true
 console.log(containsDuplicate([1,2,3])) //false
+
+*/
 /*
 function containsDuplicateSet(nums: number[]): boolean {
     const set = new Set<number>(nums);
@@ -24,3 +26,16 @@ function containsDuplicateSet(nums: number[]): boolean {
 
 console.log(containsDuplicateSet([1,2,3,1]))
 */
+
+
+/*Anagram*/
+const validAnagram = (s:string, t:string): string => {
+
+    let res = s.split("").sort().join() === t.split("").sort().join()
+    //split -- strring into array, sport array , join into string, no mas array
+    return "fin";
+};
+
+
+console.log(validAnagram("ana", "hola"));
+//console.log(validAnagram("s", "s"));
