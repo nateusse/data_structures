78563cc10742effa2cc24178aa7854c1924491b5
nateusse <natyeusse@gmail.com>
2024-02-16 15:48:28 -0500

    array python

--- python/Arrays.py
+++ python/Arrays.py
@@ -1,11 +1,19 @@
-arr = [1,2,3,4,5]
-print("con print: ", arr)
+
+arri = [1,2,3,4,5]
+print("con print: ", arri)
+
+print(arri[3:-1], arri[:3], arri[:], arri[::-1])
 
 print("con enhance loop: ")
-for element in arr: print(element,end=' ')
+for element in arri: print(element,end=' ')
 
 double =[ [1,2],[3,4], [5,6] ]
 print("Double aray: ",double)
 
 
-
+'''
+import array as arr
+b = arr.array('d', [2.5, 3.2, 3.3])
+for i in range(0, 3):
+    print(b[i], end=" ")
+'''
