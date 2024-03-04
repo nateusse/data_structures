89f9f0c89a8e294179711343e6a1297ff04c1a61
nateusse <natyeusse@gmail.com>
2024-03-04 17:10:54 -0500

    hash maps

--- python/leetcode/maxSubArray.py
+++ python/leetcode/maxSubArray.py
@@ -2,18 +2,41 @@
 Given an integer array nums, 
 find the subarray with the largest sum, 
 and return its sum
+
 """
 
+#brute force
+class Solution:
+    def maxSubArray(self, nums):
+        ans = -inf
+        for i in range(len(nums)):
+            cur_sum = 0
+            for j in range(i, len(nums)):
+                cur_sum += nums[j]
+                ans = max(ans, cur_sum)
+        return ans
+    
+#Memizacion
+class Solution:
+    def maxSubArray(self, nums):
+        @cache
+        def solve(i, must_pick):
+            if i >= len(nums): return 0 if must_pick else -inf
+            return max(nums[i] + solve(i+1, True), 0 if must_pick else solve(i+1, False))
+        return solve(0, False)
+
+#Kadane dinamic
 class Solution:
     def maxSubArray(self, nums: list[int]) -> int:
+
         if not nums:
             return 0
-        max_sum = nums[0]
-        for i in range(len(nums)):
-            current_sum = 0
-            for j in range(i, len(nums)):
-                current_sum += nums[j]
-                max_sum = max(max_sum, current_sum)
+
+        cur_sum = max_sum = nums[0]
+        for num in nums[1:]:
+            cur_sum = max(num, cur_sum + num)
+            max_sum = max(max_sum, cur_sum)
+        
         return max_sum
 
 
@@ -34,3 +57,13 @@ def maxSubArray(nums):
             max_global = max_current
     return max_global
 """
+
+class Solution:
+    def maxSubArray(self, nums: list[int]) -> int:
+        sumMax = current = nums[0]
+        
+        for i in range(len(nums)-1):
+            current = max(current+nums[i+1], nums[i+1])
+            sumMax = max(current, sumMax)
+        
+        return sumMax
---
+++ python/leetcode/valid_sudoku.py
@@ -0,0 +1,35 @@
+"""
+Determine if a 9 x 9 Sudoku board is valid. 
+Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+
+"""
+
+class Solution:
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        block = [[set() for _ in range(3)] for _ in range(3)]
+
+        for i in range(9):
+            for j in range(9):
+                curr = board[i][j]
+                if curr == '.':
+                    continue
+                if (curr in rows[i]) or (curr in cols[j]) or (curr in block[i // 3][j // 3]):
+                    return False
+                rows[i].add(curr)
+                cols[j].add(curr)
+                block[i // 3][j // 3].add(curr)
+        return True
+    
+print(Solution().isValidSudoku([["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]])) # [0, 2] 1 + 4 = 5
+print(Solution().isValidSudoku([["8","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]])) # [0, 2] 1 + 4 = 5
