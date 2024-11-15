65336f30614fd6e67f6383f6f51e32cfc493a09e
nateusse <natyeusse@gmail.com>
2024-11-15 10:40:57 -0500

    Fibonacci, factorial , static methods

--- python/recursion/Factorial.py
+++ python/recursion/Factorial.py
@@ -1,11 +1,13 @@
 class Factorial:
 
-    def factorial(self, n: int) -> int:  
+
+    @staticmethod
+    def factorial( n: int) -> int:  
         if n < 0: raise ValueError("Factorial is not defined for negative numbers")
         if n == 1 or n == 0: return 1
         
         # Recursive implementation of n! (n-factorial) calculation
-        return n * self.factorial(n - 1)
+        return n * Factorial.factorial(n - 1)
     
     #Time O(n), Space O(n)
     
